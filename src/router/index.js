90714038d29562e4bd5a26ae7998872bfd9aef64
foresthu2022 <foresthu2021@163.com>
2022-03-23 15:08:00 +0800

    项目初始化-调整初始目录结构

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+//index.js
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+vue.use(VueRouter)
+
+//路由配置表
+const routes = []
+
+const router = new VueRouter({
+	routes
+})
+
+export default router
